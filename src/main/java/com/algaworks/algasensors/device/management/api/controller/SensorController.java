22a130a7d3ad91c949491517e7544b0a1b5f3dcc
lucianolucas <luciano.lucas@bllc.com.br>
2025-05-22 14:57:28 -0300

    10.4. Implementando a persistência com Jakarta Persistence e Spring Data JPA

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -3,25 +3,32 @@ package com.algaworks.algasensors.device.management.api.controller;
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/sensors")
+@RequiredArgsConstructor
 public class SensorController {
+
+    private final SensorRepository sensorRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Sensor create(@RequestBody SensorInput sensor) {
-        return Sensor.builder()
-                .id(IdGenerator.generateTSID())
-                .name(sensor.getName())
-                .ip(sensor.getIp())
-                .protocol(sensor.getProtocol())
-                .location(sensor.getLocation())
-                .model(sensor.getModel())
+    public Sensor create(@RequestBody SensorInput input) {
+        Sensor sensor = Sensor.builder()
+                .id(new SensorId(IdGenerator.generateTSID()))
+                .name(input.getName())
+                .ip(input.getIp())
+                .protocol(input.getProtocol())
+                .location(input.getLocation())
+                .model(input.getModel())
                 .enabled(false)
                 .build();
+        return sensorRepository.saveAndFlush(sensor);
     }
 
 }
